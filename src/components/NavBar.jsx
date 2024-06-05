319985c34b3b3b02ef4c738fa9ecc4c7b7548111
Facundo Lemo <facundolemo@gmail.com>
2024-06-05 17:13:43 -0300

    better search bar on NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,16 @@ import IconoCarrito from "./svgs/IconoCarrito"
 import Lupa from "./svgs/Lupa"
 
 const NavBar = ({ getInputUser }) => {
-  const [input, setInput] = useState(null)
+  const [input, setInput] = useState(null),
+    [isFocused, setIsFocused] = useState(false);
+
+  // agrego states para el outline de la barra de búsqueda
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
 
   const handleInput = (e) => {
     setInput(e.target.value)
@@ -20,16 +29,20 @@ const NavBar = ({ getInputUser }) => {
           <img src="/mercado-libre-logo-1.png" alt="mercadolibre logo" className="w-[110px]" />
         </a>
       </div>
-      <div className="flex flex-row my-3 sm:my-0 w-full lg:w-1/2 sm:w-1/3 bg-white border-1 rounded h-7">
-        <form action="" onSubmit={handleSubmit} className="flex-grow pl-2">
-          <input type="text" placeholder="Buscar productos, marcas y más..." className="w-full" onChange={(e) => {
-            handleInput(e)
-          }}/>
+      <div className="my-2 sm:my-0 w-5/6 lg:w-1/2 sm:w-1/3 ">
+        <form action="" className={`flex items-center py-2 bg-white border-1 rounded-sm h-10 shadow-md  ${isFocused ? ' outline outline-1 outline-blue-500' : ''}`}>
+          <input type="text" required
+            className="w-full pl-3 placeholder-gray-300 focus:outline-none"
+            placeholder="Buscar productos, marcas y más..."
+            onFocus={handleFocus} onBlur={handleBlur}
+            onChange={(e) => {
+              handleInput(e)
+            }} />
+          <p className="text-gray-300 mx-4">|</p>
+          <a href="" onClick={handleSubmit} className="mr-2">
+            <Lupa />
+          </a>
         </form>
-        <p className="text-gray-300 mx-4">|</p>
-        <a href="" onClick={handleSubmit} className="mr-2">
-          <Lupa />
-        </a>
       </div>
       <div>
         <a href="/carrito">
