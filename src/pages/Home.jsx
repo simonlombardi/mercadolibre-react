2f5e2f60abb80951c49320350c64d29671493f7e
Facundo Lemo <facundolemo@gmail.com>
2024-06-11 00:37:59 -0300

    added pagination and sorting, changed some styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,35 @@ import ProductCard from '../components/ProductCard';
 import { useEffect, useState } from 'react';
 
 function Home() {
-	const [inputUser, setInputUser] = useState('boca')
-	const [data, setData] = useState(null)
-	const [isSearch, setIsSearch] = useState(false)
-	const [offset, setOffset] = useState(0)
+	const [inputUser, setInputUser] = useState('boca'),
+		[data, setData] = useState(null),
+		[isSearch, setIsSearch] = useState(false),
+		[offset, setOffset] = useState(0),
+		[contadorPagina, setContadorPagina] = useState(1),
+		[sort, setSort] = useState('');
 
 	const getInputUser = (inputUser) => {
 		setInputUser(inputUser)
 		setIsSearch(true)
+		setOffset(0)
+		setContadorPagina(1)
+		setSort('')
+	}
+	const handleSort = (sortId) => {
+		setSort(sortId)
+		setOffset(0)
+		setContadorPagina(1)
+	}
+	const handleOffset = (isForward, jumps) => {
+		if (isForward) {
+			setOffset(offset + jumps)
+			setContadorPagina(contadorPagina+1)
+		} else {
+			contadorPagina > 1 ? setOffset(offset - jumps) : setOffset(0)
+			contadorPagina > 1 ? setContadorPagina(contadorPagina-1) : setContadorPagina(1)
+			
+		}
+		location.hash = "#top"
 	}
 
 	useEffect(() => {
@@ -20,7 +41,7 @@ function Home() {
 		else {
 			const getData = async () => {
 				try {
-					const response = await FetchData(`https://api.mercadolibre.com/sites/MLA/search?q=${inputUser}&limit=${isSearch ? 15 : 5}&offset=${offset}`);
+					const response = await FetchData(`https://api.mercadolibre.com/sites/MLA/search?q=${inputUser}&limit=${isSearch ? 15 : 5}&offset=${offset}&sort=${sort}`);
 					setData(response)
 				} catch (error) {
 					console.error('Error al obtener los datos:', error);
@@ -28,29 +49,40 @@ function Home() {
 			}
 			getData();
 		}
-	}, [inputUser, offset]);
+	}, [inputUser, offset, sort]);
 
 	return (
 		<>
-			<div className='flex justify-center'>
+			<div id='top' className='flex justify-center'>
 				<NavBar getInputUser={getInputUser} />
 			</div>
 			{!isSearch && <img className='' src="/banner.jpg" alt="banner oferta día del padre" />}
 			<div className={`flex flex-col items-center ${!isSearch ? 'bg-gradient-to-b from-[#DEF0FF] to-[#EDEDED]' : 'bg-[#EDEDED]'}`}>
-				{isSearch ? <h1>Resultados de búsqueda.</h1> : <h1>Estás en la homepage.</h1>}
-				<div className='flex'>
-					<button onClick={() => setOffset(offset - 5)} className=' rotate-180 pb-1'>⮞⮞⮞</button>
-					<h1>Mover artículos</h1>
-					<button onClick={() => setOffset(offset + 5)}> ⮞⮞⮞ </button>
+				{isSearch ? <><h1 >Resultados de búsqueda.</h1><p className='font-bold'>Filtros</p></> : <h1>Estás en la homepage.</h1>}
+				<div className='flex '>
+					{isSearch ?
+						data.available_sorts.map((sort) => (
+							<p key={sort.id} onClick={() => { handleSort(sort.id) }} className=' mx-2 cursor-pointer'>{sort.name}</p>
+						))
+						: ''}
 				</div>
 			</div>
-			<div className='bg-[#EDEDED] min-h-screen flex justify-center'>
-				<div className="flex flex-wrap  w-full justify-around py-3 container">
-					{data ? data.results.map((product) => (
-						<ProductCard key={product.id} product={product} />
-					)) : <div>cargando...</div>}
+			<div className='bg-[#EDEDED] min-h-screen '>
+				<div className='flex justify-center'>
+					<div className="flex flex-wrap  w-full justify-around py-3 container">
+						{data ? data.results.map((product) => (
+							<ProductCard key={product.id} product={product} />
+						)) : <div>cargando...</div>}
+					</div>
+
+				</div>
+				<div className='flex justify-center'>
+					<button onClick={() => handleOffset(false, (isSearch ? 15 : 5))} className=' rotate-180 pb-1'>⮞⮞⮞</button>
+					<h1>Productos de la página {contadorPagina}</h1>
+					<button onClick={() => handleOffset(true, (isSearch ? 15 : 5))}> ⮞⮞⮞ </button>
 				</div>
 			</div>
+
 		</>
 	)
 }
