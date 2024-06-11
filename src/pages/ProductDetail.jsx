af9835fbc525a5771d8060cda5ee9f648603d418
simonlombardi <simon_lombardi@itecriocuarto.org.ar>
2024-06-11 20:52:30 -0300

    hotfix api response

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import FetchData from "../components/FetchData"
+import NavBar from "../components/NavBar"
 
 
 const ProductDetail = () => {
     const { id } = useParams()
     const [data, setData] = useState(null)
-    useEffect(() => {
-        const getData = async () => {
-            try {
-                const response = await FetchData(`https://api.mercadolibre.com/items/${id}`)
-                console.log(response);
-                setData(response)
-            } 
-            catch (error) {
-                console.error(`Error al obtener el dato del producto ${id}:`, error)
+
+    const getData = async () => {
+                try {
+                    const response = await FetchData(`https://api.mercadolibre.com/items/${id}`)
+                    console.log(response.title);
+                    setData(response)
+                } 
+                catch (error) {
+                    console.error(`Error al obtener el dato del producto ${id}:`, error)
+                }
             }
-        }
+console.log(data);
+    useEffect(() => {
         getData()
     }, [])
+
     return(
-        <h1>{data.title}</h1>
+        <>
+            <NavBar />
+            <div>
+                <h1>{data ? data.title : "cargando..."}</h1>
+            </div>
+        </>
     )
 }
 
