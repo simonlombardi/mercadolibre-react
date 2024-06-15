80d6f19548adc6e5deee0800d7cc9ca77230c085
simonlombardi <simon_lombardi@itecriocuarto.org.ar>
2024-06-15 12:27:34 -0300

    add funcionality to get quantity with Select component

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,11 +1,15 @@
 
+const Select = ({ getQuantity }) => {
 
-const Select = () => {
+    const handleClick = (e) => {
+        getQuantity(e.target.value)
+    }
+
     return(
         <>
             <form className=" items-center flex flex-row justify-start ">
             <label htmlFor="quantity" class="font-medium text-md pr-2">Cantidad:</label>
-            <select id="quantity" class="bg-green-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#3483fa] dark:border-[#3483fa] dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <select id="quantity" onClick={handleClick} class="bg-green-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-[#3483fa] dark:border-[#3483fa] dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 <option defaultValue={1} value='1'>1</option>
                 <option value='2'>2</option>
                 <option value='3'>3</option>
@@ -13,7 +17,6 @@ const Select = () => {
                 <option value='5'>5</option>
             </select>
             </form>
-            
         </>
     )
 }
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -4,19 +4,22 @@ import FetchData from "../components/FetchData"
 import NavBar from "../components/NavBar"
 import Spinner from "../components/Spinner"
 import Select from "../components/Select"
-
+import getInputUser from "../pages/Home"
 
 const ProductDetail = () => {
     const { id } = useParams(),
         [data, setData] = useState(null),
         [principalImg, setPrincipalImg] = useState(null),
-        [description, setDescription] = useState(null)
+        [description, setDescription] = useState(null),
+        [quantity, setQuantity] = useState(0)
 
     const getData = async () => {
         try {
             const response = await FetchData(`https://api.mercadolibre.com/items/${id}`)
             setData(response)
-            console.log(response)
+            if (response.initial_quantity > 0) {
+                setQuantity(1)
+            }
             setPrincipalImg(response.pictures[0].url)
         }
         catch (error) {
@@ -34,6 +37,10 @@ const ProductDetail = () => {
         }
     }
 
+    const getQuantity = (selectQuantity) => {
+        setQuantity(selectQuantity)
+    }
+
     const handleImage = (e) => {
         setPrincipalImg(e.target.src)
     }
@@ -45,7 +52,7 @@ const ProductDetail = () => {
 
     return (
         <>
-            <NavBar />
+            <NavBar getInputUser={getInputUser} />
             {!data ? <Spinner /> :
                 <div className=" h-screen flex justify-center items-center flex-col bg-gray-300">
                     <div className="w-10/12 overflow-y-auto mt-5 flex flex-col sm:flex-row flex-grow sm:flex-grow-0 bg-gray-50">
@@ -68,10 +75,9 @@ const ProductDetail = () => {
                                 {data.original_price ? <div className="flex"><p className="text-md line-through font-light inline">${data.original_price}</p><p className="text-[#31B771] ml-2 text-xs">DESCUENTO</p> </div> : <></>}
                                 <h2 className="text-2xl font-light ">${data.price}</h2>
                             </div>
-
                             <h4 className="font-bold">{(data.initial_quantity) > 0 ? `Stock disponible (${data.initial_quantity})` : "No hay stock"}</h4>
-                            <Select />
-                            <button class="bg-blue-500 h-12 w-full px-2 hover:bg-blue-700 text-white self-center rounded">
+                            <Select getQuantity = {getQuantity}/>
+                            <button onClick={()=>console.log(data, quantity)} class="bg-blue-500 h-12 w-full px-2 hover:bg-blue-700 text-white self-center rounded">
                                 Agregar al carrito
                             </button>
                             <div className="block">
@@ -83,7 +89,7 @@ const ProductDetail = () => {
                                     <p className="text-md text-[#5386D3] inline m-0 p-0">{data.warranty.substring(0, data.warranty.indexOf(":") + 1)}</p>
                                     <p className="text-[#929292] inline font-light">{data.warranty.substring(data.warranty.indexOf(":") + 1)}</p>
                                 </div>
-                                : <p className="text-[#929292] inline font-light">Este producto no ofrece garantía.</p>}
+                                : <p className="text-[#929292] pb-4 inline font-light">Este producto no ofrece garantía.</p>}
                         </div>
                     </div>
                     {!description ? <Spinner /> :
