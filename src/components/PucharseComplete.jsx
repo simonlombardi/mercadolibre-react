1170c4c37808592cf3d41991245d95ab5418cc2b
simonlombardi <simon_lombardi@hotmail.com>
2024-06-19 21:47:56 -0300

    add pucharse complete message

---
+++ src/components/PucharseComplete.jsx
@@ -0,0 +1,14 @@
+
+
+const PucharseComplete = () => {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center flex-col">
+            <div className="w-6/12 h-96 bg-[#EDEDED] flex justify-evenly items-center flex-col">
+                <img src="public/verificado.png" className="w-36" alt="imagen de verificacion" />
+                <p className="text-center text-xl">¡La compra se realizo correctamente!</p>
+            </div>
+            
+        </div> 
+    )
+}
+export default PucharseComplete
